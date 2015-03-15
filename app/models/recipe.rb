d23df0dc184b97e8412a054d7ba8292751d21a80
alexgeogea <alexandra.geogea@gmail.com>
2015-03-15 13:59:52 +0200

    Added recipes with ingredients and directions in nested forms.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,15 @@
 class Recipe < ActiveRecord::Base
+	has_many :ingredients
+	has_many :directions
+
+	accepts_nested_attributes_for :ingredients,
+								  reject_if: proc { |attributes| attributes['name'].blank? },
+								  allow_destroy: true
+	accepts_nested_attributes_for :directions,
+								  reject_if: proc { |attributes| attributes['step'].blank? },
+								  allow_destroy: true
+
+	validates :title, :description, :picture, presence: true
+	
 	mount_uploader :picture, PictureUploader
 end
